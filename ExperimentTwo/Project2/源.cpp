f987b61c7bcf5b655bf226ab264eb44f0839205c
Gnnnn <barbarav0101@gmail.com>
2018-05-05 02:21:49 +0800

    plz

--- ExperimentTwo/Project2/源.cpp
+++ ExperimentTwo/Project2/源.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <omp.h>
 #include <stdio.h>
 int main() {
@@ -13,3 +14,20 @@ int main() {
 		}
 	}
 }
+=======
+#include <omp.h>
+#include <stdio.h>
+int main() {
+	int nthreads, tid;
+	omp_set_num_threads(10);
+#pragma omp parallel private(nthreads,tid)
+	{
+		tid = omp_get_thread_num();
+		printf("Hello World from OMP thread %d\n", tid);
+		if (tid == 0) {
+			nthreads = omp_get_num_threads();
+			printf("Number of threads is %d\n", nthreads);
+		}
+	}
+}
+>>>>>>> f159b6d8a5f0cbbcdce24e04581e65ba9f02f08d
--- ExperimentTwo/demo/demo.c
+++ ExperimentTwo/demo/demo.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<iostream>
 
 using namespace std;
@@ -12,4 +13,20 @@ cin.get();//这个可以不要
 
 return 0;
 
+=======
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "hello world!"<< endl;
+
+cin.get();//这个可以不要
+
+return 0;
+
+>>>>>>> bf495998fd00f612bf59c79fc56d0e5a40420b01
 }
